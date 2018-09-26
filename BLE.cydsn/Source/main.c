dce7a72b7101fceaaa6e7d9db465395d8dbfef5d
Carlos Silva <clsilva78@gmail.com>
2018-09-26 16:32:29 -0500

    RGB Test.

--- BLE.cydsn/Source/main.c
+++ BLE.cydsn/Source/main.c
@@ -3,8 +3,26 @@
 int main(void)
 {
     CyGlobalIntEnable;
-    
+
+    uint8 b = 0;
+    uint8 g = 63;
+    uint8 r = 127;
+
+    PWM_BLUE_Start();
+    PWM_GREEN_Start();
+    PWM_RED_Start();
+
+    PWM_BLUE_WriteCompare(0);
+    PWM_GREEN_WriteCompare(0);
+    PWM_RED_WriteCompare(0);
+
     for (;;) {
+        for (int i = 0; i < 255; ++i) {
+            PWM_BLUE_WriteCompare(b++);
+            PWM_GREEN_WriteCompare(g++);
+            PWM_RED_WriteCompare(r++);
+            CyDelay(5);
+        }
     }
 }
 
